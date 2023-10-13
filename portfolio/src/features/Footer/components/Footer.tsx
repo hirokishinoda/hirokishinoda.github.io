941db3b8f00a7128a222b72ca6b10fb373ec981f
hirokishinoda <progac27jhisi@gmail.com>
2023-10-13 20:30:43 +0900

    Footerの作成

--- portfolio/src/features/Footer/components/Footer.tsx
+++ portfolio/src/features/Footer/components/Footer.tsx
@@ -1,6 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import { AppBar, Typography } from "@mui/material";
+import TwoLines from "components/TwoLines";
+
 const Footer = () => {
+    const footerCSS = css`
+        height  : 140px;
+    `;
+
+    const linesCSS = css`
+        margin-top: 10px;
+    `;
+
+    const copyrightCSS = css`
+        margin-top: 65px;
+        text-align: center;
+    `;
+
     return (
-       <footer></footer>
+       <AppBar component="footer" position="static" color='secondary' css={footerCSS}>
+            <Typography variant="caption" css={copyrightCSS}>
+                &copy; 2023 Hiroki Shinoda
+            </Typography>    
+            <TwoLines css={linesCSS}/>   
+        </AppBar>
     );
 }
 
