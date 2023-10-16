3b538d8f56ead4a6393e4ec133359bebce584571
hirokishinoda <progac27jhisi@gmail.com>
2023-10-16 21:19:51 +0900

    連絡先の追加

--- portfolio/src/features/Contact/components/Contact.tsx
+++ portfolio/src/features/Contact/components/Contact.tsx
@@ -1,8 +1,43 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import KaggleIcon from "assets/img/kaggle_icon.png";
+import { Avatar, Box, Link } from "@mui/material";
+import { ColorConstants } from "types/color";
+
 import ContentsEntire from "components/ContentsEntire";
 
 const Contact = () => {
+    
+    const iconCSS = css`
+        margin           : 5px;
+        background-color : ${ColorConstants.JINZAMOMI};
+    `;
+
+    const divCSS = css`
+        display : flex;
+    `;
+
+
     return (
         <ContentsEntire id="Contact" title="Contact">
+            <Box component="div" css={divCSS}>
+                <Link href="mailto:progac27jhisi@gmail.com">
+                    <Avatar css={iconCSS}>
+                        <EmailIcon />
+                    </Avatar>
+                </Link>
+                <Link href="https://github.com/hirokishinoda">
+                    <Avatar css={iconCSS}>
+                        <GitHubIcon />
+                    </Avatar>
+                </Link>
+                <Link href="https://github.com/hirokishinoda">
+                    <Avatar src={KaggleIcon} css={iconCSS} />
+                </Link>  
+            </Box>
             
         </ContentsEntire>
     );
