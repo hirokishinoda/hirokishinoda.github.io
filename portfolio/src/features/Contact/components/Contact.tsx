6444666a37a05e38853e7f582dea980aba474a99
hirokishinoda <progac27jhisi@gmail.com>
2023-10-16 21:22:12 +0900

    リンクの修正

--- portfolio/src/features/Contact/components/Contact.tsx
+++ portfolio/src/features/Contact/components/Contact.tsx
@@ -24,17 +24,17 @@ const Contact = () => {
     return (
         <ContentsEntire id="Contact" title="Contact">
             <Box component="div" css={divCSS}>
-                <Link href="mailto:progac27jhisi@gmail.com">
+                <Link href="mailto:progac27jhisi@gmail.com" underline="none" target="_blank" rel="noopener noreferrer">
                     <Avatar css={iconCSS}>
                         <EmailIcon />
                     </Avatar>
                 </Link>
-                <Link href="https://github.com/hirokishinoda">
+                <Link href="https://github.com/hirokishinoda" underline="none" target="_blank" rel="noopener noreferrer">
                     <Avatar css={iconCSS}>
                         <GitHubIcon />
                     </Avatar>
                 </Link>
-                <Link href="https://github.com/hirokishinoda">
+                <Link href="https://github.com/hirokishinoda" underline="none" target="_blank" rel="noopener noreferrer">
                     <Avatar src={KaggleIcon} css={iconCSS} />
                 </Link>  
             </Box>
